d3f463e843d3e54e0b0f78fbbdacd460518c1575
c1arkchin <chincla@uw.edu>
2022-11-13 14:29:40 -0800

    added csv

---
+++ source/calc_sum_info.R
@@ -0,0 +1,12 @@
+library("tidyverse")
+
+netflix_stats <- read_csv("Documents/info201/projects/
+                          project-group-5-section-ah/data/
+                          netflix-rotten-tomatoes-metacritic-imdbcsv.csv")
+
+revenue <- read_csv("Documents/info201/projects/
+                    project-group-5-section-ah/data/
+                    statistic_id187069_box-office-revenue-in-the-us-and-canada-1980-2021.csv")
+
+top_500_movies <- read_csv("Documents/info201/projects/
+                           project-group-5-section-ah/data/top-500-movies.csv")
