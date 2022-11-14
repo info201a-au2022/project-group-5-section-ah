3cecb62f462069aa30be2b271402d7a4a2a54d1b
parkersh1 <psh1@uw.edu>
2022-11-14 11:31:44 -0800

    small changes

--- source/calc_sum_info.R
+++ source/calc_sum_info.R
@@ -8,23 +8,23 @@ netflix_stats <- read_csv("https://raw.githubusercontent.com/info201a-au2022/pro
 
 #number of movies in Netflix dataset
 netflix_movie_number <- nrow(netflix_stats)
-netflix_movie_number <- paste0("There are", netflix_movie_number, "movies in the Netflix dataset.")
+netflix_movie_number <- paste0("There are", netflix_movie_number, "movies in the Netflix dataset. ")
 
 #number of unique genre combinations in the Netflix dataset
 unique_genres <- length(unique(netflix_stats$Genre))
-unique_genres <- paste0("There are", unique_genres, "unique genre combinations in the Netflix dataset.")
+unique_genres <- paste0("There are", unique_genres, "unique genre combinations in the Netflix dataset. ")
 
 #year with max movie revenue in U.S. and Canada
 year_max_revenue <- revenue %>% 
   filter(...2 == max(...2, na.rm = TRUE)) %>% 
   pull(`Box office revenue in the U.S. and Canada 1980-2021`)
-year_max_revenue <- paste0(year_max_revenue, "was the year with the max movie revenue in the U.S. and Canada")
+year_max_revenue <- paste0(year_max_revenue, " was the year with the max movie revenue in the U.S. and Canada. ")
 
 #movie with highest IMDb score
 highest_imdb_score <- netflix_stats %>% 
   filter(`IMDb Score` == max(`IMDb Score`, na.rm = TRUE)) %>% 
     pull(Title)
-highest_imdb_score <- paste0("The movie with the highest IMDb rating is ", highest_imdb_score)
+highest_imdb_score <- paste0("The movie with the highest IMDb rating is ", highest_imdb_score, ". ")
 highest_imdb_score
 
 #movie with highest production cost
@@ -32,28 +32,25 @@ highest_production_cost <- top_500_movies %>%
   select(title, production_cost) %>% 
   filter(production_cost == max(production_cost)) %>% 
   pull(title)
-highest_production_cost <- paste0("The movie with the highest production cost is ", highest_production_cost)
-
+highest_production_cost <- paste0("The movie with the highest production cost is ", highest_production_cost, ". ")
 
 #movie with highest domestic gross 
 highest_domestic_gross <- top_500_movies %>% 
   select(title, domestic_gross) %>% 
   filter(domestic_gross == max(domestic_gross)) %>% 
   pull(title)
-highest_domestic_gross <- paste0("The movie with the highest domestic gross is ", highest_domestic_gross)
+highest_domestic_gross <- paste0("The movie with the highest domestic gross is ", highest_domestic_gross, ". ")
 
 #movie with highest opening weekend gross 
 highest_opening_weekend <- top_500_movies %>% 
   select(title, opening_weekend) %>% 
   filter(opening_weekend == max(opening_weekend, na.rm = TRUE)) %>% 
   pull(title)
-highest_opening_weekend <- paste0("The movie with the highest opening weekend gross is ", highest_opening_weekend)
+highest_opening_weekend <- paste0("The movie with the highest opening weekend gross is ", highest_opening_weekend, ". ")
 
 #highest occuring genre
 frequent_genre <- sort(table(top_500_movies$genre), decreasing = TRUE)[1:10]
 frequent_genre
-
-
 
 
 
