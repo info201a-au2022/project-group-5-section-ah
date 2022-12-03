5958d40fc96a4c57bb40f6d6cd6656a7b11d3567
c1arkchin <chincla@uw.edu>
2022-12-03 14:52:07 -0800

    changes

--- shiny/app_server.R
+++ shiny/app_server.R
@@ -1,3 +1,12 @@
 library(shiny)
 library(ggplot2)
 library(tidyverse)
+
+server = function(input, output) {
+  output$txtout <- renderText({
+    paste(input$txt, input$slider, format(input$date), sep = ", ")
+  })
+  output$table <- renderTable({
+    head(cars, 4)
+  })
+}
