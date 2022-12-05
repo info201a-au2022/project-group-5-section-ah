3ae016511710be8312a421a13be61f9d22cdb4b5
c1arkchin <chincla@uw.edu>
2022-12-05 10:43:29 -0800

    changes

--- project-template1/tabs/tab_panel_chart1.R
+++ project-template1/tabs/tab_panel_chart1.R
@@ -4,7 +4,7 @@ library(shiny)
 
 tab_panel_chart1 <-tabPanel(
   "Visualization 1",
-  p("This is chart 1."),
+  p("This chart expresses the relationship between the production cost of a given movie title and the actual Worldwide gross of said movie. This comparison is meant to describe the size of correlation between the amount a director decided to spend on a movie and how much that investment (or lack of investment) affected the overall profitability of said movie title. As we can see, the movie with the highest production cost is Avengers: Endgame, which is also the second most grossed movie. Additionally, the movie titled: Avatar, had a production budget/cost of only half that of Avengers: Endgame, but still managed to gross more money. This means that the worldwide gross of a movie may not be as tied to the production cost of a movie as we had initially thought."),
   fluidPage(
     titlePanel("Visualization 1"),
     sidebarLayout(position = "right",
